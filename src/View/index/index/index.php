c84a2dfa5387b8965ab688967fa30ebe2b1ab8db
Jon Schwartz <jon@mediaspike.net>
2018-06-06 15:03:42 -0400

    finishing up

--- src/View/index/index/index.php
+++ src/View/index/index/index.php
@@ -1,3 +1,33 @@
 <?php
 $this->layout('layout/layout', ['title' => $title]);
-echo 'Hello this is a template';
+?>
+<?php $this->start('jumbotron'); ?>
+<div class="jumbotron">
+    <h1>Hi!  I'm Jon.<hr class="blurb-underline"/></h1>
+</div>
+<?php $this->end(); ?>
+<div class="intro-blurb">
+    <p>Hi Everyone!  I'm Jon.  I'm a web developer living outside of boston.  I like building tools to help people work better faster easier.  I usually code in PHP but I also like Python and have an extensive MySQL background.</p>
+    <div class="mini-res">
+        <h3>What I've done</h3>
+        <p class="res-item">Web Developer - Mediaspike 2012-2018</p>
+        <p class="res-item">Web and Marketing Manager - HMC Electronics 2009-2012</p>
+        <p class="res-item">Software Engineer - MediRegs 2006-2009</p>
+        <h3>Certifications</h3>
+        <p class="res-item"><a href="http://www.zend.com/en/yellow-pages/ZEND026277">Zend Certified PHP Engineer - achieved 2013</a></p>
+        <h3>Talks</h3>
+        <p class="res-item">Turn your house into a giant robot - given at NortheastPHP 2017</p>
+        <h3>School</h3>
+        <p class="res-item">Bridgewater State College (2001-2006)</p>
+        <p class="res-sub-item">BS in Computer Science</p>
+        <p class="res-sub-item">BS in Psychology</p>
+    </div>
+</div>
+<div class="social-share">
+    <h1>SAY HELLO</h1>
+    <a href="mailto:jon@jon-schwartz.com" title="Email Me"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+    <a href="https://www.twitter.com/jonschwartz" title="Twitter" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+    <a href="https://www.github.com/jonschwartz" title="Github" target="_blank"><i class="fab fa-github" aria-hidden="true"></i></a>
+    <a href="https://www.linkedin.com/in/jonathanhenryschwartz/" title="LinkedIn" target="_blank"><i class="fab fa-linkedin" aria-hidden="true"></i></a>
+    <a href="https://docs.google.com/document/d/1FM21_MMWYnhL8kphwJKaiaU-vU6eEdXdiE-Bj-Q2OeQ/edit?usp=sharing" class="resume-link" target="_blank" title="My Resume"><i class="fab fa-google-drive"></i></a>
+</div>
--- src/View/layout/layout.php
+++ src/View/layout/layout.php
@@ -2,16 +2,20 @@
     <head>
         <title><?php echo $title; ?></title>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link href="/css/style.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <link href="/css/style.css" rel="stylesheet"/>
     </head>
     <body>
-        <div class="container">
+        <div class="container-fluid">
+            <?php echo $this->section('jumbotron'); ?>
+        </div>
+        <div class="container-fluid">
             <?php echo $this->section('content'); ?>
         </div>
 
         <div class="container">
             <footer>
-                &copy; Jon Schwartz
+                &copy; <?php echo date("Y"); ?> Jon Schwartz
             </footer>
         </div>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
